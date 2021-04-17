b8c98a3ee9ce97935f5da0744eb3ef0630355870
alongre <alongre@gmail.com>
2021-04-17 08:42:09 +0300

    initialize firebase auth logouta and user

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,13 @@
 import { FunctionComponent, ReactNode } from "react";
 import Link from "next/link";
-// import { useAuth } from "src/auth/useAuth";
+import { useAuth } from "src/auth/useAuth";
 
 interface IProps {
   main: ReactNode;
 }
 
 const Layout: FunctionComponent<IProps> = ({ main }) => {
-  const authenticated = false;
-  const logout = () => {};
+  const { user, authenticated, logout } = useAuth();
   return (
     <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
       <nav className="bg-gray-800" style={{ height: "64px" }}>
